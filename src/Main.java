2b74a25b70273ae892e15f7323afc0de53415d4b
Marina Santiago <marinasantiago1718@gmail.com>
2025-10-23 18:38:05 -0300

    QuickSort

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,24 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        // teste do heapSort 
+        System.out.println("Quick Sort:");
+        double[] numeros = {8, 3, 1, 7, 0, 10, 2};
+        QuickSort qs = new QuickSort(numeros);
+
+        System.out.println("Antes: " + qs);
+        qs.sort(0, numeros.length - 1);
+        System.out.println("Depois: " + qs);
+        // teste do heapSort
+        System.out.println("\nHeap Sort:");
         int arr[] = {12, 11, 13, 5, 6, 7};
-        
+
         System.out.println("Array original: " + Arrays.toString(arr));
 
         HeapSort ob = new HeapSort();
         ob.sort(arr);
 
         System.out.println("Array ordenado (Heap Sort): " + Arrays.toString(arr));
-        
+
         int arr2[] = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
         System.out.println("\nArray original 2: " + Arrays.toString(arr2));
         ob.sort(arr2);
--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,2 +1,51 @@
 public class QuickSort {
+    private double[] array;
+
+    public QuickSort(double[] array) {
+        this.array = array;
+    }
+
+    public void sort(int inicio, int fim){
+        if (inicio < fim) {
+            int posPivo = divideArray(array, inicio, fim);
+            sort(inicio, posPivo - 1);
+            sort(posPivo + 1, fim);
+        }
+
+    }
+
+    public int divideArray(double[] array, int inicio, int fim){
+
+        double pivo = array[fim];
+        int indice = inicio;
+        for(int i= inicio; i<fim; i++){
+            if(array[i] < pivo){
+                double store = array[i];
+                array[i] = array[indice];
+                array[indice] = store;
+                indice++;
+            }
+        }
+
+        double store = array[indice];
+        array[indice] = pivo;
+        array[fim] = store;
+
+        return indice;
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+            if (i < array.length - 1) sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+
 }

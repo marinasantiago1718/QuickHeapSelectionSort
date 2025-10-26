019c020741fb7c4f72a819c352bd2cb222e36c02
talespool <tales.pimentel@edu.unifor.br>
2025-10-26 14:55:33 -0300

    Analise Tempo e Espaço

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -11,14 +11,13 @@ public class QuickSort {
             sort(inicio, posPivo - 1);
             sort(posPivo + 1, fim);
         }
-
     }
 
     public int divideArray(double[] array, int inicio, int fim){
 
         double pivo = array[fim];
         int indice = inicio;
-        for(int i= inicio; i<fim; i++){
+        for(int i = inicio; i<fim; i++){
             if(array[i] < pivo){
                 double store = array[i];
                 array[i] = array[indice];
@@ -32,7 +31,6 @@ public class QuickSort {
         array[fim] = store;
 
         return indice;
-
     }
 
     @Override
@@ -46,6 +44,41 @@ public class QuickSort {
         sb.append("]");
         return sb.toString();
     }
-
-
 }
+
+/* Complexidade de Tempo
+ * - Melhor caso: T(n) = 2T(n/2) + n => Θ(nlogn), segundo caso mestre.
+ *  Ocorre quando o pivô é a mediana, dividindo o array igualmente.
+ * 
+ * - Pior caso: T(n) = T(n-1) + n => Θ(n²), ficaria uma árvore de altura n e cn = n, logo n².
+ *  Árvore: n             -
+ *        0   n-1         |
+ *           0  n-2       | h = n
+ *              0  ...    |
+ *                   1    -
+ *                   cn = n, então n*n => n².
+ * 
+ *  Ocorre quando o pivô é sempre o menor ou o maior elemento, neste caso seria o maior, em um array já ordenado.
+ * 
+ * - Caso médio: T(n) = T(n/10) + T(9n/10) + n => Θ(nlogn), ficaria uma árvore de altura log n e cn = n, logo nlogn.
+ *  Árvore:    n                   - (9/10)^0 * n 
+ *     n/10           9n/10        | (9/10)^1 * n
+ * n/100  9n/100  9n/100  81n/100  | (9/10)^2 * n                  
+ *     ...              ...        |     
+ *      1                1         - h => (9/10)^h * n => h = log n
+ *                                                              10/9
+ *                                  cn = n, logo n*log n = Θ(nlogn)
+ * 
+ * Complexidade de Espaço
+ *  A função que faz a partição do array, divideArray(), tem complexidade de espaço Θ(1), pois ela cria apenas varáveis locais.
+ * 
+ *  Método sort(), a complexidade de espaço depende da profundidade da recursão, que está relacionada com a altura da árvore
+ * 
+ *  - Pior caso: Θ(n), pois a pilha de chamadas cresce linearmente (árvore de altura n).
+ * 
+ *  - Melhor caso: Θ(log n), pois a árvore é balanceada (altura log n).
+ * 
+ *  - Caso médio: Θ(log n), pois a profundidade esperada da pilha é log n.
+ * 
+*/
+
